a42ef28c4a79cd48d88b64471882e89741a97f40
Dan McDade <dmcdade@MAC-DMCDADE.local>
2019-01-15 16:31:20 -0500

    adding my vueapp quickstarter

---
+++ vue-projects/vueapp01-quickstart/src/components/HelloWorld.vue
@@ -0,0 +1,111 @@
+<template>
+<div class="hello">
+  <h1>{{ msg }}</h1>
+  <div>
+    <ul>
+      <div v-for="user in users" v-bind:key=user.id>
+        <li>{{ user.firstName }} {{ user.lastName }}</li>
+      </div>
+    </ul>
+  </div>
+  <div>
+    <input type="text" v-model="input_val">
+  </div>
+  <div>
+    Input value: <span v-text="input_val"></span>
+  </div>
+  <button class="btn btn-primary" v-on:click="counter++">You've clicked this
+    button {{ counter }} times!</button>
+  <h2>Essential Links</h2>
+  <ul>
+    <li>
+      <a href="https://vuejs.org" target="_blank">
+        Core Docs
+      </a>
+    </li>
+    <li>
+      <a href="https://forum.vuejs.org" target="_blank">
+        Forum
+      </a>
+    </li>
+    <li>
+      <a href="https://chat.vuejs.org" target="_blank">
+        Community Chat
+      </a>
+    </li>
+    <li>
+      <a href="https://twitter.com/vuejs" target="_blank">
+        Twitter
+      </a>
+    </li>
+    <br>
+    <li>
+      <a href="http://vuejs-templates.github.io/webpack/" target="_blank">
+        Docs for This Template
+      </a>
+    </li>
+  </ul>
+  <h2>Ecosystem</h2>
+  <ul>
+    <li>
+      <a href="http://router.vuejs.org/" target="_blank">
+        vue-router
+      </a>
+    </li>
+    <li>
+      <a href="http://vuex.vuejs.org/" target="_blank">
+        vuex
+      </a>
+    </li>
+    <li>
+      <a href="http://vue-loader.vuejs.org/" target="_blank">
+        vue-loader
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/vuejs/awesome-vue" target="_blank">
+        awesome-vue
+      </a>
+    </li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      users: [
+        {id: 1, firstName: 'Dan', lastName: 'L'},
+        {id: 2, firstName: 'Anan', lastName: 'L'}
+      ],
+      input_val: '',
+      counter: 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
