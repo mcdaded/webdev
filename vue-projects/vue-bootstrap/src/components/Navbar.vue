d41c842cdab289519cfd89539eb0a54ad6607849
Dan McDade <dmcdade@MAC-DMCDADE.local>
2019-03-09 13:49:37 -0500

    bootstrap styling

---
+++ vue-projects/vue-bootstrap/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="navbar" class="navbar">
+    <b-nav pills>
+      <b-nav-item active>Active</b-nav-item>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <b-nav-item>Link</b-nav-item>
+      <b-nav-item>Another Link</b-nav-item>
+      <b-nav-item disabled>Disabled</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
+
+<style scoped>
+.navbar {
+  background-color: blue;
+}
+</style>
