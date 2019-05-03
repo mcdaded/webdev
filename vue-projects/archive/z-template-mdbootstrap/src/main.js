ab6e0ec86cb820e633024f456c22d2e5f8824f5c
Dan McDade <daniel.r.mcdade@gmail.com>
2019-05-02 22:14:43 -0400

    cleaning up folders a little

---
+++ vue-projects/archive/z-template-mdbootstrap/src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbvue/build/css/mdb.css";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
