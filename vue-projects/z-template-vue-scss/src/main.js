966c9a04ba13d2e02c7de2c9915861f33bf17665
Dan McDade <daniel.r.mcdade@gmail.com>
2019-05-16 22:17:33 -0400

    some buefy changes

--- vue-projects/z-template-vue-scss/src/main.js
+++ vue-projects/z-template-vue-scss/src/main.js
@@ -2,9 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import Buefy from "buefy";
-
-Vue.use(Buefy);
 
 Vue.config.productionTip = false;
 
