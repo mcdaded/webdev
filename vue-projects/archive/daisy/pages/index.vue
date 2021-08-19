ccb55fbc5e7dd7aa1ac490f7d32a239e93dfdbaa
Dan McDade <daniel.r.mcdade@gmail.com>
2021-08-19 18:14:56 -0400

    updates for dev stuff

---
+++ vue-projects/archive/daisy/pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container py-24">
+    <div>
+      <!-- <div class="hero min-h-screen bg-base-200">
+        <div class="flex-col hero-content lg:flex-row-reverse">
+          <img
+            src="https://picsum.photos/id/1005/600/600"
+            class="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+            <p class="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div> -->
+      <h1 class="text-5xl">daisy</h1>
+
+      <label for="my-modal-2" class="btn btn-primary modal-button"
+        >open modal</label
+      >
+      <input type="checkbox" id="my-modal-2" class="modal-toggle" />
+      <div class="modal">
+        <div class="modal-box">
+          <p>
+            Enim dolorem dolorum omnis atque necessitatibus. Consequatur aut
+            adipisci qui iusto illo eaque. Consequatur repudiandae et. Nulla ea
+            quasi eligendi. Saepe velit autem minima.
+          </p>
+          <div class="modal-action">
+            <label for="my-modal-2" class="btn btn-primary">Accept</label>
+            <label for="my-modal-2" class="btn">Close</label>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data: () => ({
+    showModal: false,
+  }),
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+</style>
---
+++ vue-projects/archive/glass-sample-tailwind/components/Modal.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="items-center px-5 py-12 lg:px-20">
+    <div
+      class="
+        w-full
+        modal-glass
+        px-5
+        mx-auto
+        border
+        rounded-lg
+        shadow-xl
+        lg:px-0
+        text-blueGray-500
+        lg:w-full
+      "
+      aria-hidden="false"
+      aria-describedby="modalDescription"
+      role="dialog"
+    >
+      <div class="flex items-center justify-end px-6 py-4">
+        <button
+          class="
+            p-1
+            transition-colors
+            duration-200
+            transform
+            rounded-md
+            hover:bg-opacity-25 hover:bg-blueGray-600
+            focus:outline-none
+          "
+          type="button"
+          aria-label="Close"
+          aria-hidden="true"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon icon-tabler icon-tabler-circle-x"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+            <circle cx="12" cy="12" r="9"></circle>
+            <path d="M10 10l4 4m0 -4l-4 4"></path>
+          </svg>
+        </button>
+      </div>
+      <div class="flex flex-col w-full mb-12 text-center">
+        <h2
+          class="
+            mb-8
+            text-xs
+            font-semibold
+            tracking-widest
+            text-black
+            uppercase
+            title-font
+          "
+        >
+          a great header right here
+        </h2>
+        <h1
+          class="
+            mx-auto
+            mb-8
+            text-2xl
+            font-semibold
+            leading-none
+            tracking-tighter
+            text-black
+            lg:w-1/2
+            sm:text-4xl
+            title-font
+          "
+        >
+          A Long headline to switch your visitors into users.
+        </h1>
+        <a
+          class="mx-auto text-sm font-semibold text-blue-600 hover:text-black"
+          title="read more"
+        >
+          Read more about the offer »
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+}
+</script>
+
+<style>
+.modal-glass {
+  background: rgba(61, 179, 168, 0.3);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(6px);
+  -webkit-backdrop-filter: blur(6px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+</style>
